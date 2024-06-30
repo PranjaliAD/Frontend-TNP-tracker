f948ccbb0510eb182b701732e5e59d7d23d35eb5
PranjaliAD <pranjalidharmadhikari2002@gmail.com>
2024-06-30 12:01:28 +0530

    deplo

--- src/Components/Pages/Instructor_profile/StaticsticStudent.jsx
+++ src/Components/Pages/Instructor_profile/StaticsticStudent.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { PieChart, PieArcSeries, PieArcLabel } from '@mui/x-charts';
+// import React from 'react';
 
-const data = [
-  { id: 0, value: 8, label: 'placed' },
-  { id: 1, value: 2, label: 'Unplaced' },
-  // { id: 2, value: 20, label: 'series C' },
-];
+// const data = [
+//   { id: 0, value: 8, label: 'placed' },
+//   { id: 1, value: 2, label: 'Unplaced' },
+//   // { id: 2, value: 20, label: 'series C' },
+// ];
 
-export default function StaticsticStudent() {
-  return (
-    <PieChart
-    colors={['#5c6bc0','pink']} 
-      series={[
-        {
-          data,
-          highlightScope: { faded: 'global', highlighted: 'item' },
-          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-        },
-      ]}
-      height={200}
-    />
-  );
-}
+// export default function StaticsticStudent() {
+//   return (
+//     <PieChart
+//     colors={['#5c6bc0','pink']} 
+//       series={[
+//         {
+//           data,
+//           highlightScope: { faded: 'global', highlighted: 'item' },
+//           faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+//         },
+//       ]}
+//       height={200}
+//     />
+//   );
+// }
